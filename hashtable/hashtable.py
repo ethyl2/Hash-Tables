7c12a1edab93e512a00edbc880694035aced20d7
Heather Nuffer <zzzuzz@gmail.com>
2020-05-04 13:38:02 -0600

    Adds code to new hashtable function

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -17,6 +17,11 @@ class HashTable:
     Implement this.
     """
 
+    def __init__(self, capacity):
+        self.capacity = capacity  # Number of buckets in the hash table
+        self.storage = [None] * capacity
+        self.initial_capacity = capacity
+
     def fnv1(self, key):
         """
         FNV-1 64-bit hash function
@@ -30,13 +35,17 @@ class HashTable:
 
         Implement this, and/or FNV-1.
         """
+        hash = 5381
+        for x in key:
+            hash = ((hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
 
-    def hash_index(self, key):
+    def _hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -47,6 +56,21 @@ class HashTable:
 
         Implement this.
         """
+        self.size_check()
+        hashed_key = self._hash_index(key)
+        new_linked_pair = HashTableEntry(key, value)
+
+        node = self.storage[hashed_key]
+        if node is None:
+            self.storage[hashed_key] = new_linked_pair
+            return
+
+        while node is not None and node.key != key:
+            prev = node
+            node = node.next
+
+        if node is None:
+            prev.next = new_linked_pair
 
     def delete(self, key):
         """
@@ -56,6 +80,31 @@ class HashTable:
 
         Implement this.
         """
+        # Get the hashed_key.
+        hashed_key = self._hash_index(key)
+
+        # Get the value stored in storage at that hashed_key.
+        node = self.storage[hashed_key]
+
+        # If that node is the desired one, point the storage[hashed_key] to its next.
+        if node.key == key:
+            self.storage[hashed_key] = node.next
+            self.size_check()
+            return
+
+        # Traverse the LL until the key is found or the end of the LL is reached.
+
+        while node is not None and node.key != key:
+            prev = node
+            node = node.next
+
+        if node is None:
+            print(f'{key} was not found')
+            return None
+        # Remove the LinkedPair node from the chain by assigning
+        # the .next pointer of the previous node to be the node that its .next pointer was pointing to.
+        prev.next = node.next
+        self.size_check()
 
     def get(self, key):
         """
@@ -65,6 +114,35 @@ class HashTable:
 
         Implement this.
         """
+        # Compute hash
+        hashed_key = self._hash_index(key)
+
+        # Get the first node in LL in storage
+        node = self.storage[hashed_key]
+
+        # Traverse the linked list at this node until the key is found or the end is reached
+        while node is not None and node.key != key:
+            node = node.next
+
+        if node is None:
+            return None
+        else:
+            return node.value
+
+    def make_new_storage(self):
+        new_storage = [None] * self.capacity
+
+        for i in range(len(self.storage)):
+            # print(f'at index {i} in self.storage')
+            node = self.storage[i]
+
+            while node is not None:
+                # traverse the LL to rehash each key/value pair
+                # print("At key: " + str(node.key))
+                hashed_key = self._hash_index(node.key)
+                new_storage[hashed_key] = node
+                node = node.next
+        self.storage = new_storage
 
     def resize(self):
         """
@@ -73,6 +151,39 @@ class HashTable:
 
         Implement this.
         """
+        self.capacity = self.capacity * 2
+        self.make_new_storage()
+
+    def shrink(self):
+        '''
+        Halves the capacity of the hash table and
+        rehashes all key/value pairs.
+        '''
+        self.capacity = self.capacity // 2
+        self.make_new_storage()
+
+    def size_check(self):
+        '''
+        Update your HashTable to automatically double in size when it grows past a load factor of 0.7
+        and half in size when it shrinks past a load factor of 0.2.
+        This (I assume the halving) should only occur 
+        if the HashTable has been resized past the initial size.
+
+        '''
+        num_entries = sum(x is not None for x in self.storage)
+        # print('num_entries: ' + str(num_entries))
+        load_factor = num_entries/self.capacity
+        # print('load factor: ' + str(load_factor))
+
+        if load_factor > 0.7:
+            # print('Time to increase the capacity; load factor is ' + str(load_factor))
+            self.resize()
+
+        if self.capacity > self.initial_capacity:
+            if load_factor < 0.2:
+                # print('time to shrink the hashtable in half')
+                self.shrink()
+
 
 if __name__ == "__main__":
     ht = HashTable(2)
