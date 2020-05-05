0577a702a272aae4f56dfe47162ded0fef18f962
Heather Nuffer <zzzuzz@gmail.com>
2020-05-05 17:16:01 -0600

    Uses dictionary for looking up values for printing in sumdiff.py

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -14,9 +14,11 @@ f(7) + f(1) = f(12) - f(1)    34 + 10 = 54 - 10
 f(3) + f(3) = f(12) - f(3)    18 + 18 = 54 - 18
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
+# q = set(range(1, 10))
+# q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
+# q = set(range(1, 50))
+# q = set(range(1, 25))
 
 
 def f(x):
@@ -31,13 +33,7 @@ combos = product(q, repeat=4)
 result_dict = dict()
 
 for line in combos:
-    # print(line)
     # Calculate left side and right side and see if they are equal
-
-    first = 0
-    second = 0
-    third = 0
-    fourth = 0
 
     # For each call of f(), check first to see if result is in the dictionary
     if result_dict.get(line[0]):
@@ -64,11 +60,5 @@ for line in combos:
         fourth = f(line[3])
         result_dict[line[3]] = fourth
 
-    left_side = first + second
-    right_side = third - fourth
-    # print(f"{left_side} vs {right_side}")
-    if left_side == right_side:
-        # f(1) + f(1) = f(12) - f(7)    10 + 10 = 54 - 34
-        # print('found one')
-        print(
-            f"f({line[0]}) + f({line[1]}) = f({line[2]}) - f({line[3]})  {f(line[0])} + {f(line[1])} = {f(line[2])} - {f(line[3])}")
+    if first + second == third - fourth:
+        print(f"f({line[0]}) + f({line[1]}) = f({line[2]}) - f({line[3]})  {result_dict[line[0]]} + {result_dict[line[1]]} = {result_dict[line[2]]} - {result_dict[line[3]]}")
